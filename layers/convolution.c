5a0db9fe6a80d8480349769217c8cf9bd701d3df
mauris-winata <maurissius@gmail.com>
2021-03-15 22:59:32 -0400

    adding conv, max pool layer

---
+++ layers/convolution.c
@@ -0,0 +1,103 @@
+// #include <algorithm>
+#include <float.h>
+#include "layer_defs.h"
+#include <stdint.h>
+
+void conv_layer(int32_t * mem,         // global memory pointer
+				int32_t * input, 		// where to get inputs
+				int32_t * output,		// where to store outputs
+				int offset, 			// offset for biases, weights
+                // int input_offset,       // offset of inputs
+                // int output_offset,      // offset of outputs
+                const int b,            // batch size
+                const int od,           // number of filters
+                const int ox,           // output width
+                const int oy,           // output height
+                const int id,           // number of inputs 
+                const int ix,           // input width
+                const int iy,           // input height
+                const int s,            // stride
+                const int k)            // kernel size
+{
+
+// // Global memory interface
+// #pragma HLS INTERFACE m_axi port=mem depth=2147483648
+// // Bind all control ports to a single bundle
+// #pragma HLS INTERFACE s_axilite port=b bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=od bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=ox bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=oy bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=id bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=ix bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=iy bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=s bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=k bundle=CTRL_BUS
+// #pragma HLS INTERFACE s_axilite port=input_offset
+// #pragma HLS INTERFACE s_axilite port=output_offset
+// #pragma HLS INTERFACE s_axilite port=return bundle=CTRL_BUS
+ 
+  int num_weights = id*od*k*k;
+  int num_biases = od;
+  int num_input = b*id*ix*iy;
+  int num_output = b*od*ox*oy;
+  
+  //Calculating padding required
+  // int left_pad;
+  // int right_pad; 
+  // int top_pad;
+  // int bot_pad;
+  // int horz_padding;
+  // int vert_padding; 
+  
+  // horz_padding = ((ix/k + 1)*k)%ix;
+  // vert_padding = ((iy/k + 1)*k)%iy;
+  
+
+  // Batch
+  for (int b_=0; b_< b; b_++)
+  {
+    // Number of filters
+    for (int o_d = 0; o_d < od; o_d++)
+    {
+      // Output Y Dimension
+      for (int o_y = 0; o_y < oy; o_y++)
+      {
+        // Output X Dimension
+        for (int o_x = 0; o_x < ox; o_x++)
+        {
+          // Set bias
+		   uint32_t bias = mem[offset/sizeof(uint32_t) + num_weights + o_d]; //one bias per filter
+           float output_element = bias;
+
+          // Weighted Sum:
+
+          // Number of inputs
+          for (int i_d = 0; i_d < id; i_d++)
+          {
+            // Input Y Dimension
+            for (int i_y = o_y*s-k/2, iiy = 0; i_y < o_y*s+k-s; i_y++, iiy++)
+            {
+              if (i_y < 0) continue;
+              if (i_y > iy - 1) continue;
+              // Input X Dimension
+              for (int i_x = o_x*s-k/2, iix = 0; i_x < o_x*s+k-s; i_x++, iix++)
+              {
+                
+                if (i_x < 0) continue;
+                if (i_x > ix - 1) continue;
+				//access the input array; offset is: batch + current input + row + column
+				uint32_t input_val = input[b_*id*ix*iy + i_d*ix*iy + i_y*ix + i_x];
+				
+				//access the weights/bias array; offset is: + row + column
+				uint32_t weight = mem[offset/sizeof(uint32_t) + o_d*k*k + iiy*k + iix];
+                output_element += input_val*weight;                                  
+              }
+            }
+          }
+          // Write output
+          output[b_*od*ox*oy + o_d*ox*oy + o_y*ox + o_x] = MAX(0, output_element); //ReLu activation means output is non-negative
+        }
+      }
+    }
+  }
+}
---
+++ layers/layer_test.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "layer_defs.h"
+
+#define DIM 5
+
+int main(){
+    int kernel_size = 2;
+    int num_filters = 1;
+    int num_weights_biases = num_filters*kernel_size*kernel_size + num_filters;
+    int32_t weights_biases[10] = {1,1,1,1,1,1,1,1,1,0};
+    int32_t input_arr[DIM*DIM] = {1,1,2,4,5,5,6,7,8,3,3,2,1,0,7,1,2,3,4,8,4,6,5,9,4};
+    int32_t output_arr[DIM*DIM];
+    int32_t output_arr_2[3*3];
+    
+    //First conv layer
+    conv_layer(weights_biases,         	  // global memory pointer
+				input_arr, 				  // where to get inputs
+				output_arr,				  // where to store outputs
+				0, 						  // offset for biases, weights
+                1,            			  // batch size
+                CONV1_NUM_FILTERS,        // number of filters
+                CONV1_OUTPUT_X,           // output width
+                CONV1_OUTPUT_Y,           // output height
+                CONV1_NUM_INPUTS,         // number of inputs 
+                CONV1_OUTPUT_X,           // input width
+                CONV1_OUTPUT_Y,           // input height
+                CONV1_STRIDE,             // stride
+                CONV1_KERNEL);            // kernel size
+    
+    int i,j;
+	
+    printf("Kernel array:\n");
+    for(i = 0; i < CONV1_KERNEL; i++){
+        for (j = 0; j < CONV1_KERNEL; j++){
+            printf("%d ", weights_biases[j+ CONV1_KERNEL*i]);  
+        }
+        printf("\n");
+    }
+	
+    printf("Input array:\n");
+    for(i = 0; i < DIM; i++){
+        for (j = 0; j < DIM; j++){
+            printf("%d ", input_arr[j+ DIM*i]);  
+        }
+        printf("\n");
+    }
+    
+    printf("Output array:\n");
+    for(i = 0; i < DIM; i++){
+        for (j = 0; j < DIM; j++){
+            printf("%d ", output_arr[j+ DIM*i]);  
+        }
+        printf("\n");
+    }
+    
+    max_pool(output_arr, 		// where to get inputs
+    			output_arr_2,		// where to store outputs
+                1,           // number of outputs
+                3,           // output width
+                3,           // output height
+                5,           // input width
+                5);           // input height    
+ 
+    printf("Output array 2:\n");
+    for(i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            printf("%d ", output_arr_2[j+ 3*i]);  
+        }
+        printf("\n");
+    }   
+    
+    printf("Hello World");
+
+    return 0;
+}
