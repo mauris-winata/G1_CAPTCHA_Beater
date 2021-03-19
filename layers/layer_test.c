9d945128b3989c164bc5a30468ea4114760aba04
Srivatsan <srivat97@outlook.com>
2021-03-19 15:53:51 -0400

    added testing for the dense layer

--- layers/layer_test.c
+++ layers/layer_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "layer_defs.h"
+#include "dense_layer.h"
 
 #define DIM 5
 
@@ -12,7 +13,12 @@ int main(){
     int32_t input_arr[DIM*DIM] = {1,1,2,4,5,5,6,7,8,3,3,2,1,0,7,1,2,3,4,8,4,6,5,9,4};
     int32_t output_arr[DIM*DIM];
     int32_t output_arr_2[3*3];
-    
+
+    // inputs and outputs to verify the dense layer
+    // input to dense layer is flattened
+    float input_array_dense[INPUT_LENGTH_DENSE_LAYER_TEST] = { 1,2,3,4 };
+    float output_array_dense[OUTPUT_LENTH_DENSE_LAYER_TEST] = {0};
+    float weights_biases_test[10] = { 1,1,1,1,1,1,1,1,1,0 };
     //First conv layer
     conv_layer(weights_biases,         	  // global memory pointer
 				input_arr, 				  // where to get inputs
@@ -69,6 +75,43 @@ int main(){
         }
         printf("\n");
     }   
+    
+    // dense layer test output
+
+    printf("**************************************** Beginning Dense Layer Testing ****************************************\n");
+
+    printf("Kernel array for dense layer test:\n");
+    for (i = 0; i < CONV1_KERNEL; i++) {
+        for (j = 0; j < CONV1_KERNEL; j++) {
+            printf("%d ", weights_biases[j + CONV1_KERNEL * i]);
+        }
+        printf("\n");
+    }
+
+    printf("Input array for dense layer test:\n");
+    for (i = 0; i < INPUT_LENGTH_DENSE_LAYER_TEST; i++) 
+    {
+        printf("%f ", input_array_dense[i]);
+    }
+
+    printf("\n");
+
+    // calling the dense layer function
+    dense_layer(weights_biases_test,
+        input_array_dense,
+        output_array_dense,
+        0,
+        1,
+        INPUT_LENGTH_DENSE_LAYER_TEST,
+        OUTPUT_LENTH_DENSE_LAYER_TEST);
+
+    printf("output array for dense layer test:\n");
+    for (i = 0; i < OUTPUT_LENTH_DENSE_LAYER_TEST; i++)
+    {
+        printf("%f ", output_array_dense[i]);
+    }
+
+    printf("\n**************************************** Finishing Dense Layer Testing ****************************************\n");
     
     printf("Hello World");
 
