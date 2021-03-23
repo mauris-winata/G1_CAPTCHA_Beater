8414805915a9cd53263da425cfaa040f3074dc8e
mauris-winata <maurissius@gmail.com>
2021-03-22 22:53:56 -0400

    corrected

--- layers/batch_norm_layer.h
+++ layers/batch_norm_layer.h
@@ -14,8 +14,17 @@
 #define BATCH_NORM_2_OUTPUT_X BATCH_NORM_2_INPUT_X
 #define BATCH_NORM_2_OUTPUT_Y BATCH_NORM_2_INPUT_X
 
-// Batch norm 1
+// Batch norm 3
 #define BATCH_NORM_3_INPUT_X CONV3_OUTPUT_X
 #define BATCH_NORM_3_INPUT_Y CONV3_OUTPUT_Y
 #define BATCH_NORM_3_OUTPUT_X BATCH_NORM_3_INPUT_X
 #define BATCH_NORM_3_OUTPUT_Y BATCH_NORM_3_INPUT_X
+
+void batch_norm(float * mem,         // global memory pointer (stores params)
+				float * input, 		// where to get inputs
+				float * output,		// where to store outputs
+				int offset, 			// offset for biases, weights
+                const int b,            // number of batches
+                const int id,           // number of inputs (number of outputs from previous conv layer)
+                const int ix,           // input width
+                const int iy);           // input height
