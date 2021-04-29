13eec79aca7c746551b2c4a7a12a80387d2bceb5
Srivatsan <srivat97@outlook.com>
2021-04-29 18:06:31 -0400

    Added max pool dense layer IP files, test files and IP generation scripts

---
+++ layers/dense_test/max_pool_dense_test_hls.h
@@ -0,0 +1,53 @@
+// this file contains parmeters and functions that are used to test the dense layer after the final max pool layer
+
+#ifndef MAX_POOL_DENSE_TEST_HLS_H
+#define MAX_POOL_DENSE_TEST_HLS_H
+
+#include <stdio.h>
+#include "../dense/max_pool_dense_layer_hls.h"
+#include "../util/shared.h"
+
+// First dense layer set, layer 1
+//#define MAX_POOL_DENSE_LAYER_1_TEST_INPUT_DATA "../../../../../golden_outputs/max_pooling2d_3_output.txt" // will use previous layers output when tetsing entire model
+#define MAX_POOL_DENSE_LAYER_1_TEST_INPUT_DATA "../../../../../generated_outputs/max_pool_3.txt" 
+#define MAX_POOL_DENSE_LAYER_1_TEST_OUTPUT_DATA "../../../../../generated_outputs/max_pool_dense_1.txt"
+#define MAX_POOL_DENSE_LAYER_1_TEST_WEIGHTS_BIAS "../../../../../weights/dense_1_weights.txt"
+#define MAX_POOL_DENSE_LAYER_1_TEST_GOLDEN_OUTPUT "../../../../../golden_outputs/dense_1_output.txt"
+#define MAX_POOL_DENSE_LAYER_1_NAME "Max_pool_dense_1"
+
+// First dense layer set, layer 2
+//#define MAX_POOL_DENSE_LAYER_2_TEST_INPUT_DATA "../../../../../golden_outputs/max_pooling2d_3_output.txt" // will use previous layers output when tetsing entire model
+#define MAX_POOL_DENSE_LAYER_2_TEST_INPUT_DATA "../../../../../generated_outputs/max_pool_3.txt" 
+#define MAX_POOL_DENSE_LAYER_2_TEST_OUTPUT_DATA "../../../../../generated_outputs/max_pool_dense_2.txt"
+#define MAX_POOL_DENSE_LAYER_2_TEST_WEIGHTS_BIAS "../../../../../weights/dense_3_weights.txt"
+#define MAX_POOL_DENSE_LAYER_2_TEST_GOLDEN_OUTPUT "../../../../../golden_outputs/dense_3_output.txt"
+#define MAX_POOL_DENSE_LAYER_2_NAME "Max_pool_dense_2"
+
+// First dense layer set, layer 3
+//#define MAX_POOL_DENSE_LAYER_3_TEST_INPUT_DATA "../../../../../golden_outputs/max_pooling2d_3_output.txt" // will use previous layers output when tetsing entire model
+#define MAX_POOL_DENSE_LAYER_3_TEST_INPUT_DATA "../../../../../generated_outputs/max_pool_3.txt" 
+#define MAX_POOL_DENSE_LAYER_3_TEST_OUTPUT_DATA "../../../../../generated_outputs/max_pool_dense_3.txt"
+#define MAX_POOL_DENSE_LAYER_3_TEST_WEIGHTS_BIAS "../../../../../weights/dense_5_weights.txt"
+#define MAX_POOL_DENSE_LAYER_3_TEST_GOLDEN_OUTPUT "../../../../../golden_outputs/dense_5_output.txt"
+#define MAX_POOL_DENSE_LAYER_3_NAME "Max_pool_dense_3"
+
+// First dense layer set, layer 4
+//#define MAX_POOL_DENSE_LAYER_4_TEST_INPUT_DATA "../../../../../golden_outputs/max_pooling2d_3_output.txt" // will use previous layers output when tetsing entire model
+#define MAX_POOL_DENSE_LAYER_4_TEST_INPUT_DATA "../../../../../generated_outputs/max_pool_3.txt" 
+#define MAX_POOL_DENSE_LAYER_4_TEST_OUTPUT_DATA "../../../../../generated_outputs/max_pool_dense_4.txt"
+#define MAX_POOL_DENSE_LAYER_4_TEST_WEIGHTS_BIAS "../../../../../weights/dense_7_weights.txt"
+#define MAX_POOL_DENSE_LAYER_4_TEST_GOLDEN_OUTPUT "../../../../../golden_outputs/dense_7_output.txt"
+#define MAX_POOL_DENSE_LAYER_4_NAME "Max_pool_dense_4"
+
+// First dense layer set, layer 5
+//#define MAX_POOL_DENSE_LAYER_5_TEST_INPUT_DATA "../../../../../golden_outputs/max_pooling2d_3_output.txt" // will use previous layers output when tetsing entire model
+#define MAX_POOL_DENSE_LAYER_5_TEST_INPUT_DATA "../../../../../generated_outputs/max_pool_3.txt" 
+#define MAX_POOL_DENSE_LAYER_5_TEST_OUTPUT_DATA "../../../../../generated_outputs/max_pool_dense_5.txt"
+#define MAX_POOL_DENSE_LAYER_5_TEST_WEIGHTS_BIAS "../../../../../weights/dense_9_weights.txt"
+#define MAX_POOL_DENSE_LAYER_5_TEST_GOLDEN_OUTPUT "../../../../../golden_outputs/dense_9_output.txt"
+#define MAX_POOL_DENSE_LAYER_5_NAME "Max_pool_dense_5"
+
+
+void max_pool_dense_layer_test(const char* input_data, const char* weights, const char* golden_output_data, const char* output_data, int offset, const char* layer_name, layer_params max_pool_dense_layer_parameters);
+
+#endif
---
+++ layers/hls_proj/max_pool_dense_layer_four.tcl
@@ -0,0 +1,29 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
+############################################################
+cd hls_proj
+open_project max_pool_dense_layer_four.prj
+set_top max_pool_dense_layer_four
+add_files ../util/fixed_point.cpp -cflags "-std=c++0x"
+add_files ../dense/max_pool_dense_layer_four.cpp -cflags "-std=c++0x"
+add_files -tb ../util/fixed_point.cpp -cflags "-std=c++0x -Wno-unknown-pragmas"
+add_files -tb ../layer_test.cpp -cflags "-std=c++0x -Wno-unknown-pragmas"
+add_files -tb ../dense_test/max_pool_dense_test.cpp -cflags "-std=c++0x -Wno-unknown-pragmas"
+add_files -tb ../util/shared.cpp -cflags "-std=c++0x -Wno-unknown-pragmas"
+open_solution "solution1"
+set_part {xcvu095-ffvc1517-2-e} -tool vivado
+create_clock -period 4 -name default
+config_compile -no_signed_zeros=0 -unsafe_math_optimizations=0
+#config_sdx -optimization_level none -target none
+config_bind -effort medium
+config_schedule -effort medium -relax_ii_for_timing=0
+#config_export -format ip_catalog -rtl verilog
+set_clock_uncertainty 12.5%
+#source "./max_pool_dense_layer.prj/solution1/directives.tcl"
+#csim_design -clean
+csynth_design
+#cosim_design
+#export_design -flow impl -rtl verilog -format ip_catalog
+export_design -format ip_catalog
